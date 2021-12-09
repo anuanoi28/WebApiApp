49784eb9b1d72676f8900f51150c9335feff2153
anuanoi28 <antonyanu186@gmail.com>
2021-12-09 15:14:25 +0530

    Add project files.

---
+++ WebApiApp/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiApp.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        string str = "";
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Anu", "Sona" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value - " + id;
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+            str = value;
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
+
+
+// web uri's : GET, POST, PUT, DELETE
